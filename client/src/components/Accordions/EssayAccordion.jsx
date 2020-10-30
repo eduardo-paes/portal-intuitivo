1f8ca8bdcc48daa79b7ad1e54ab5ca0a60f42f4b
Eduardo <eduardo-paes@outlook.com>
2020-10-30 19:03:20 -0300

    Correções em download de pdf

--- client/src/components/Accordions/EssayAccordion.jsx
+++ client/src/components/Accordions/EssayAccordion.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 
-import { makeStyles, Grid, Button, Accordion, AccordionSummary, AccordionDetails, Typography, Avatar } from '@material-ui/core';
+import { makeStyles, Grid, Backdrop, Button, CircularProgress, Accordion, AccordionSummary, AccordionDetails, Typography, Avatar, Badge } from '@material-ui/core';
 import { SimpleRadio, UploadEssay, SimpleFeedback } from "../";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import SaveIcon from '@material-ui/icons/Save';
 import DownloadIcon from '@material-ui/icons/GetApp';
+import CheckCircleIcon from '@material-ui/icons/Check';
 import axios from "axios";
+import { jsPDF } from "jspdf";
 
 import api from '../../api';
+import { green } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
   myCard: {
@@ -23,109 +26,136 @@ const useStyles = makeStyles((theme) => ({
   userName: {
     paddingTop: '0.6rem',
     paddingLeft: '1rem'
-  }
+  },
+  success: {
+    backgroundColor: green[500],
+    color: '#fafafa',
+    borderRadius: '50%',
+    margin: '0.5rem 0 0 0.5rem'
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
 }));
+
+const loadingFlag = {
+    srcImg: false,
+    alunoID: false,
+    redacaoID: false,
+}
 
 export default function Accordions(props) {
     const { data } = props;
     const classes = useStyles();
     const [srcImg, setSrcImg] = useState('');
     const [notaAluno, setNotaAluno] = useState(0);
-    const [wasLoaded, setWasLoaded] = useState({ srcImg: false });
+    const [wasLoaded, setWasLoaded] = useState(loadingFlag);
     const [feedMsg, setFeedMsg] = useState({title: '', message: ''});
     const [feedOpen, setFeedOpen] = useState(false);
     const [essayUploaded, setEssayUploaded] = useState(false);
-
-    const alunoID = data.alunoID._id, redacaoID = data.redacaoID;
-
-    const donwloadLink = `http://localhost:5000/api/download-redacao/${alunoID}/${redacaoID}`;
-    const uploadLink = `http://localhost:5000/api/upload-redacao/corrigida/${alunoID}/${redacaoID}`;
-    const nomeRedacao = `Redação - ${data.alunoID.nome}.pdf`;
+    const [alunoID, setAlunoID] = useState('');
+    const [redacaoID, setRedacaoID] = useState('');
+    const [backdrop, setBackdrop] = useState(false);
 
     useEffect(() => {
         const abortController = new AbortController();
-        if (data.alunoID && !wasLoaded.srcImg) {
+        if (alunoID !== '' && !wasLoaded.srcImg) {
             setSrcImg(`http://localhost:5000/uploads/profile/${data.alunoID._id}.jpeg`);
-            setWasLoaded(true);
+            setWasLoaded(preValue => ({
+                ...preValue,
+                srcImg: true,
+            }));
+        }
+
+        if (data.alunoID && !wasLoaded.alunoID) {
+            setAlunoID(data.alunoID._id);
+            setWasLoaded(preValue => ({
+                ...preValue,
+                alunoID: true,
+            }));
+        }
+
+        if (data.redacaoID && !wasLoaded.redacaoID) {
+            setRedacaoID(data.redacaoID);
+            setWasLoaded(preValue => ({
+                ...preValue,
+                redacaoID: true,
+            }));
         }
         return abortController.abort();
         // eslint-disable-next-line
     }, [data]);
 
-    // const convertImgToPDF = () => {
-    //     var doc = new jsPDF();
-    //     var type = '';
-    //     if (file.type === 'image/png') {
-    //         type = 'PNG';
-    //     } else if (file.type === 'image/jpeg') {
-    //         type = 'JPEG';
-    //     }
-    //     doc.addImage("examples/images/Octonyan.jpg", type, 10, 10, 190, 190);
-    // }
+    const UploadEssayCorrection = (event) => {
+        const file = event.target.files[0];
+        setBackdrop(true);
+        
+        if (file) {
+            const formData = new FormData();
+            formData.append("foto", file);
 
-    // const pdf = new File([doc.output("blob")], "filename.pdf", {  type: "pdf" });
-    //     data = new FormData();
-    //     data.append("file", pdf);
-    //     axios
-    //         .post("/amazing-endpoint", data)
-    //         .then(res => console.log(res))
-    //         .catch(err =>console.log(err));
-
-    const fetchRedacaoIMG = async (file) => {
-
-        const formData = new FormData();
-        formData.append("foto", file);
-        return await fetch(uploadLink, {
+            fetch(`http://localhost:5000/api/upload-redacao/corrigida/${data.alunoID._id}/${data.redacaoID}`, {
                 method: 'POST',
                 body: formData
             })
             .then(res => {
                 if (res.status !== 200) {
                     setFeedMsg({
-                        title: 'Ops! Houve um erro ao enviar sua redação.',
-                        message: 'Verifique se o arquivo que nos enviou não está corrompido ou se possui um dos seguintes formatos permitidos: .jpg, .png, .jpeg.'
+                        title: 'Houve um erro ao enviar sua correção',
+                        message: 'Verifique se o arquivo que enviou não está corrompido ou se possui um dos seguintes formatos permitidos: .jpg, .png, .jpeg.'
                     })
                     setFeedOpen(true);
-                    return false;
+                } else {
+                    setFeedMsg({
+                        title: 'Correção enviada',
+                        message: 'Sua correção foi enviada com sucesso e já está disponível para o aluno'
+                    })
+                    setBackdrop(false);
+                    setFeedOpen(true);
+                    setEssayUploaded(true);
                 }
-                setFeedMsg({
-                    title: 'Redação enviada!',
-                    message: 'Aí sim! Agora é só aguardar a correção de nossos professores. Em breve você estará recebendo sua correção!'
-                })
-                return true;
             })
-    }
-
-    const UploadEssayCorrection = async (event) => {
-        const file = event.target.files[0];
-        console.log(file);
-
-        const value = await fetchRedacaoIMG(file);
-        if (value) {
-            setFeedOpen(true);
-            setEssayUploaded(true);
         }
     };
 
     const DownloadEssay = (event) => {
         event.preventDefault();
-            axios.get(donwloadLink,
-                {
-                    responseType: 'arraybuffer',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Accept': 'application/jpg'
-                    }
-                })
-                .then((response) => {
-                    const url = window.URL.createObjectURL(new Blob([response.data]));
-                    const link = document.createElement('a');
+        const downloadLink = `http://localhost:5000/api/download-redacao/${alunoID}/${redacaoID}`;
+
+        axios.get(downloadLink,
+            {
+                responseType: 'arraybuffer',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': ['application/jpg', 'application/jpeg', 'application/png', 'application/pdf',]
+                }
+            })
+            .then((response) => {
+                
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement('a');
+
+                const content = response.headers['content-type'];
+                
+                var aux = 'pdf';
+                var nomeRedacao = `Redação - ${data.alunoID.nome}.${aux}`;
+
+                if (content.includes('image/')) {
+                    aux = content.split('image/').filter(ext => {
+                        return ext !== '';
+                    });
+                    var doc = new jsPDF();
+                    doc.addImage(url, aux, 10, 7);
+                    doc.save(nomeRedacao);
+                } else {
                     link.href = url;
                     link.setAttribute('download', nomeRedacao);
                     document.body.appendChild(link);
                     link.click();
-                })
-                .catch((error) => console.log(error));
+                }
+            })
+            .catch((error) => console.log(error));
     };
 
     const SubmitButton = async (event) => {
@@ -156,8 +186,11 @@ export default function Accordions(props) {
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1a-content"
             >
-                <Avatar sizes="small" src={srcImg} alt="Preview"/>
+                <Badge overlap="circle" badgeContent={essayUploaded && <CheckCircleIcon className={classes.success} />}>
+                    <Avatar sizes="small" src={srcImg} alt="Preview"/>
+                </Badge>
                 <Typography className={classes.userName}>{data.alunoID.nome}</Typography>
+                
             </AccordionSummary>
 
             <AccordionDetails>
@@ -177,13 +210,17 @@ export default function Accordions(props) {
                             </Grid>
 
                             <Grid item={true} xs={12} sm={4}>
-                                <UploadEssay handleUpload={UploadEssayCorrection} checked={false} primaryTitle="Enviar Correção" secondaryTitle='' />
+                                <UploadEssay handleUpload={UploadEssayCorrection} alunoID={alunoID} checked={false} primaryTitle="Enviar Correção" secondaryTitle='' />
 
                                 <SimpleFeedback
                                     open={feedOpen}
                                     setOpen={setFeedOpen}
                                     title={feedMsg.title}
                                     message={feedMsg.message}/>
+
+                                <Backdrop className={classes.backdrop} open={backdrop}>
+                                    <CircularProgress color="inherit" />
+                                </Backdrop>
                             </Grid>
 
                             <Grid item={true} xs={12} sm={4}>
